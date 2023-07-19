6c43feba91a5a9186434785eb9773bf7dd4bd2b6
thiagoaxon <135708306+thiagoaxon@users.noreply.github.com>
2023-07-19 12:05:37 -0300

    first commit

---
+++ front/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Router from "svelte-spa-router";
+
+  import Login from "./routes/login.svelte";
+  import Register from "./routes/register.svelte";
+  import AddUser from "./routes/authScreens/addUser.svelte";
+  import Home from "./routes/authScreens/home.svelte";
+</script>
+
+<body>
+  <Router 
+    routes={{
+      "/": Login,
+      "/register": Register,
+      "/home": Home,
+      "/add/user": AddUser,
+    }}
+  />
+</body>
+
+<style>
+  
+</style>
